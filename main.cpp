1eb99fb34100dabe3d6fa47b936bd0797d1f7a65
wyf <1328988774@qq.com>
2024-01-04 11:58:02 +0800

    first

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char *argv[])
 {
     //需要修改的数据库信息,登录名,密码,库名
-    string user = "root";
-    string passwd = "root";
-    string databasename = "qgydb";
+    string user = "debian-sys-maint";
+    string passwd = "57Uw4RPV3lugABrT";
+    string databasename = "yourdb";
 
     //命令行解析
     Config config;
